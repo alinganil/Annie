ab348b1a218a66a9b23959ab4815d5c5954d309f
alinganil <118309882+alinganil@users.noreply.github.com>
2025-02-14 09:30:12 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ValentinePage from './ValentinePage'
+import '../styles/main.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ValentinePage />
+  </React.StrictMode>
+)
