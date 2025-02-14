50d5419f78b98da29bbeff00a8be47ceec371bf0
alinganil <118309882+alinganil@users.noreply.github.com>
2025-02-14 09:38:03 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ValentinePage from './ValentinePage'
+import ValentinePage from './ValentinePage.jsx'
 import '../styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
