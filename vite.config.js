e09691dc39a936b09a2517741c2828584ea19b66
alinganil <118309882+alinganil@users.noreply.github.com>
2025-02-14 09:52:28 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Annie/',  // Change 'Annie' to your GitHub repo name
   build: {
     outDir: 'dist',
     emptyOutDir: true
